321f59add9747541770ce06f2047f58149a01a48
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-01-10 12:32:12 -0800

    add basic view to redirect to when subscription is successful

---
+++ resources/views/subscription_success.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.layout_base')
+
+@section('content')
+    <div id="content-success-subscription">
+        <p>Hey {{$email}}!</p>
+        <p>Almost done! You should receive an email to confirm your subscription to daylammer soon!</p>
+    </div>
+@endsection
