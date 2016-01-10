13538023986e3b801d0bffce01d9fca4f320b60c
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-01-10 12:11:15 -0800

    migrations for users and confirmation_tokens attribute

---
+++ database/migrations/2016_01_10_200555_create_table_confirmation_tokens.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableConfirmationTokens extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confirmation_tokens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('token')->unique();
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('confirmation_tokens');
+    }
+}
