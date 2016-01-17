f58c526e2c53e5ba83dc2aa61237f517ca99f242
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2016-01-17 14:18:01 -0800

    add pivot table for difficulty user pivot table

---
+++ database/migrations/2016_01_17_221337_create_difficulty_user_pivot_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDifficultyUserPivotTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('difficulty_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('difficulty');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('difficulty_users');
+    }
+}
